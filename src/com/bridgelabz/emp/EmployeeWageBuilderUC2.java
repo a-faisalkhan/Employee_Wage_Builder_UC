86361850cd8d345cadec25be72279d18f2195f3e
a-faisalkhan <kfaisalit@gmail.com>
2022-02-21 00:11:04 +0530

    Check employee attendance

---
+++ src/com/bridgelabz/emp/EmployeeWageBuilderUC2.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.emp;
+
+public class EmployeeWageBuilderUC2 {
+	
+	public static void main(String[] args) {
+		final int PRESENT_NUMBER_RANGE = 2;
+		final int PRESENT = 1;
+		final int WAGE_PER_HOUR = 20;
+		final int WORKING_HOUR = 8;
+	
+	int isPresent = (int) Math.floor(Math.random() * PRESENT_NUMBER_RANGE); //(int) typecasting required becoz we are storing	
+	
+	int empWage;
+	if (isPresent == PRESENT) {
+		System.out.println("Employee is present");
+		empWage = WAGE_PER_HOUR * WORKING_HOUR;
+	} else {
+		System.out.println("Employee is absent");
+		empWage = 0; 
+	}
+	System.out.println("Emp Wage: $" + empWage +" USD");
+	}
+
+}
